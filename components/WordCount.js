960989f68ab701737c42a1655cda8e48879f7107
graphimetry <zhu22608002@outlook.com>
2026-08-22 12:30:58 +0800

    fix: Add null check for wordCount to prevent display issues

--- components/WordCount.js
+++ components/WordCount.js
@@ -6,12 +6,16 @@ import { useGlobal } from '@/lib/global'
  */
 export default function WordCount({ wordCount, readTime }) {
   const { locale } = useGlobal()
+  
+  // 如果字数为0或未定义，返回占位符或提示
+  const displayWordCount = wordCount || 0
+  
   return (
     <span id='wordCountWrapper' className='flex gap-3 font-light'>
       <span className='flex whitespace-nowrap items-center'>
         <i className='pl-1 pr-2 fas fa-file-word' />
         <span>{locale.COMMON.WORD_COUNT}</span>&nbsp;
-        <span id='wordCount'>{wordCount}</span>
+        <span id='wordCount'>{displayWordCount}</span>
       </span>
     </span>
   )
